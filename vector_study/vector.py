0b3a21c4f422519cdad4375eecba948fba4d67de
ursjj <ursjj@163.com>
2018-07-05 22:14:55 +0800

    add normallize and magnitude

--- vector_study/vector.py
+++ vector_study/vector.py
@@ -1,4 +1,4 @@
-from itertools import izip
+from math import sqrt
 
 class Vector(object):
     def __init__(self, coordinates):
@@ -7,6 +7,8 @@ class Vector(object):
                 raise ValueError
             self.coordinates = tuple(coordinates)
             self.dimension = len(coordinates)
+            self._magnitude = None
+            self._normalization = None
 
         except ValueError:
             raise ValueError('The coordinates must be nonempty')
@@ -23,10 +25,47 @@ class Vector(object):
         return self.coordinates == v.coordinates
 
     def __add__(self, v):
-        return Vector([x[0] + x[1] for x in izip(self.coordinates, v.coordinates)])
+        return Vector([x[0] + x[1] for x in zip(self.coordinates, v.coordinates)])
 
     def __sub__(self, v):
-        return Vector([x[0] - x[1] for x in izip(self.coordinates, v.coordinates)])
+        return Vector([x[0] - x[1] for x in zip(self.coordinates, v.coordinates)])
 
-    def ____(self, ):
-        return Vector([sum(x) for x in list(zip(self.coordinates, v.coordinates))])
+    def times(self, c):
+        return Vector([x * c for x in self.coordinates])
+
+    @property
+    def magnitude(self):
+        if not self._magnitude:
+            self._magnitude = sqrt(sum([x**2 for x in self.coordinates]))
+
+        return self._magnitude
+
+    @property
+    def normalization(self):
+        if not self._normalization:
+            self._normalization = self.times(1/self.magnitude)
+
+        return self._normalization
+
+    def product(self, v):
+        return sum([x[0] * x[1] for x in zip(self.coordinates, v.coordinates)])
+
+def cal_n_m():
+    v1 = Vector((-0.211, 7.437))
+    print(v1.magnitude)
+
+    v3 = Vector((5.581, -2.136))
+    print(v3.normalization)
+
+def cal_pro_agl():
+    v1=Vector((7.887, 4.138))
+    v2=Vector((-8.802, 6.776))
+
+    print(v1.product(v2))
+    v3=Vector((-5.955, -4.904, -1.874))
+    v4=Vector((-4.496, -8.755, 7.103))
+    print(v3.product(v4))
+
+
+if __name__== "__main__":
+    cal_n_m()
